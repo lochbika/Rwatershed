978f89d15043d5bb2d6abb1fec88814dab48fb20
Kai <kai_lochi@gmx.de>
2022-01-19 17:25:47 +0100

    added skeleton function

---
+++ R/watershed.R
@@ -0,0 +1,9 @@
+#' @title Watershed algorithm
+#' @description Performs the watershed algorithm on a 2d array
+#' @param x input matrix
+#' @return Returns a matrix of the same dimensions as x.
+#' @source This function is part of the following repository at \url{https://github.com/lochbika/Rwatershed}
+
+watershed <- function(x){
+
+}
