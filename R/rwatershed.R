072159e14373bec95fec860be7945c7aa3895cac
Kai <kai_lochi@gmx.de>
2022-01-27 11:35:48 +0100

    Added R implementation

---
+++ R/rwatershed.R
@@ -0,0 +1,73 @@
+#' @title An R implementation of a watershed segmentation
+#' @description This function should not be called directly. Instead, use watershed.
+#' @param x input matrix
+#' @param periodic Use periodioc boundary conditions? Default: TRUE.
+#' @return Returns a matrix of the same dimensions as x.
+#' @source This function is part of the following repository at \url{https://github.com/lochbika/Rwatershed}
+#' @export
+#' @import magic
+#' @import reshape2
+
+rwatershed <- function(x) {
+  # index of shift directions
+  # 1 2 3
+  # 4 5 6
+  # 7 8 9
+  shift.vectors <- matrix(
+    c(1, 1,   #1
+      1, 0,   #2
+      1, -1,  #3
+      0, 1,   #4
+      0, 0,   #5
+      0, -1,  #6
+      -1, 1,  #7
+      -1, 0,  #8
+      -1, -1),#9
+      ncol = 2,
+      byrow = TRUE
+    )
+    # create 9 shifted copies; 5 is the original
+    data.shifted <- array(0.0, dim = c(dim(x)[1], dim(x)[2], 9))
+    for (i in 1:9) {
+      data.shifted[, , i] <- magic::ashift(x, v = shift.vectors[i, ])
+    }
+    # get the direction to the neighbors at each pixel
+    # if a particle would have follow the gradient
+    data.direction <-
+      reshape2::melt(apply(data.shifted, FUN = which.max, MARGIN = c(1, 2)))
+    # generate initial coordinates for each particle of the input matrix
+    init.coor <- expand.grid(seq(1, dim(x)[1]), seq(1, dim(x)[2]))
+    # initialize, then loop and move particles along the gradient
+    changed <- TRUE
+    old.coor <- init.coor
+    new.coor <- init.coor
+    while (changed) {
+      # calculate new coordinates
+      new.coor[, 1] <-
+        old.coor[, 1] - shift.vectors[data.direction[(old.coor[, 2] - 1) * dim(x)[1] + old.coor[, 1], 3], 1]
+      new.coor[, 2] <-
+        old.coor[, 2] - shift.vectors[data.direction[(old.coor[, 2] - 1) * dim(x)[1] + old.coor[, 1], 3], 2]
+      # boundary checks
+      new.coor[new.coor[, 1] > dim(x)[1], 1] <- 1
+      new.coor[new.coor[, 2] > dim(x)[2], 2] <- 1
+      new.coor[new.coor[, 1] < 1, 1] <- dim(x)[1]
+      new.coor[new.coor[, 2] < 1, 2] <- dim(x)[2]
+      # check if anything changed
+      if (all(new.coor == old.coor)) {
+        changed <- FALSE
+      } else{
+        # update old.coor for next iteration
+        old.coor <- new.coor
+      }
+    }
+
+    # fill matrix with labels for segments
+    data.output <- matrix(0, nrow = dim(x)[1], ncol = dim(x)[2])
+    loc.max <- unique((old.coor[, 2] - 1) * dim(x)[1] + old.coor[, 1])
+
+    for (i in 1:length(loc.max)) {
+      data.output[seq(1, dim(x)[1] * dim(x)[2])[(old.coor[, 2] - 1) * dim(x)[1] + old.coor[, 1] ==
+                                                  loc.max[i]]] <- i
+    }
+    return(data.output)
+}
--- R/watershed.R
+++ R/watershed.R
@@ -1,25 +1,24 @@
 #' @title Watershed algorithm
-#' @description Performs the watershed algorithm on a matrix
+#' @description Performs the watershed algorithm on a matrix.
 #' @param x input matrix
-#' @param method Chose the method to use. Must be a character. Currently supported method is "simple".
-#' @param periodic Use periodioc boundary conditions? Default: False.
+#' @param method Chose the method to use. Must be a character. Currently supported implementations are "fortran" and "R".
+#' @param periodic Use periodioc boundary conditions? Default: TRUE.
 #' @return Returns a matrix of the same dimensions as x.
 #' @source This function is part of the following repository at \url{https://github.com/lochbika/Rwatershed}
 #' @export
 #' @useDynLib Rwatershed
 
 watershed <- function(x,
-                      method = "simple",
-                      periodic = FALSE) {
+                      method = "fortran",
+                      periodic = TRUE) {
   # only allow a matrix as input
   if (!is.matrix(x)) {
     stop("Error: x must be a matrix!")
   }
-  # get dimensions of input matrix for fortran
-  nx <- dim(x)[2]
-  ny <- dim(x)[1]
-  # call the fortran subroutine
-  if (method == "simple") {
+  if (method == "fortran") {# call the fortran subroutine
+    # get dimensions of input matrix for fortran
+    nx <- dim(x)[2]
+    ny <- dim(x)[1]
     retdata <- .Fortran(
       "watershed_simple",
       # the input matrix x
@@ -35,9 +34,12 @@ watershed <- function(x,
       # periodic boundary conditions?
       periodici = periodic
     )
+    data.output <- retdata$outdata
+  }else if(method == "R"){# call the R implementation
+    data.output <- rwatershed(x)
   }else{
     stop("Error: Unknown method!")
   }
   # return the data
-  return(retdata$outdata)
+  return(data.output)
 }
